dd7b88fa3d4970a85fab43c98beff631c4e56471
Ivan Dozorov <oozo1101@yandex.ru>
2022-07-02 14:45:47 +0300

    fix: edit cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,9 +12,10 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  res.header('Access-Control-Allow-Credentials', true);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
