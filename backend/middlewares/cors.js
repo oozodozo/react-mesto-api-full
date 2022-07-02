07244d6acb51a608c9b3958db14ce5a6fdf2f48e
Ivan Dozorov <oozo1101@yandex.ru>
2022-07-02 16:48:12 +0300

    fix: cors err

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,14 +9,14 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
   const { method } = req;
-  const { origin } = req.headers;
+  const origin = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
   res.header('Access-Control-Allow-Credentials', true);
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+ // if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', 'https://dozo.nomoredomains.xyz');
+ // }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
