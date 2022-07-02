82ad0d18fcbca542d693c02017a52df691a12497
Ivan Dozorov <oozo1101@yandex.ru>
2022-07-02 16:30:00 +0300

    fix: cors err

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,10 +11,8 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
-  console.log(origin);
-  console.log(method);
 
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', true);
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
