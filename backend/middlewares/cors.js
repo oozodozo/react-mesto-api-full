b1cb86fde1f8777983a33ab3088b5e59a128e873
Ivan Dozorov <oozo1101@yandex.ru>
2022-07-02 17:04:53 +0300

    feat: add new version front

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -14,9 +14,9 @@ module.exports = (req, res, next) => {
 
   res.header('Access-Control-Allow-Credentials', true);
 
- // if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', 'https://dozo.nomoredomains.xyz');
- // }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
