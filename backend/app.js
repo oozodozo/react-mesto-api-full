171be4554103645e4e620fc1d719a390b1630c61
Ivan Dozorov <oozo1101@yandex.ru>
2022-07-02 18:12:57 +0300

    fix: cors err

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,11 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cookieParser());
+app.use(cors);
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,6 +9,24 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
   const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  res.header('Access-Control-Allow-Origin', '*');
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.end();
+  }
+  next();
+};
+
+/*
+module.exports = (req, res, next) => {
+  const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
@@ -25,3 +43,4 @@ module.exports = (req, res, next) => {
   }
   next();
 };
+*/
